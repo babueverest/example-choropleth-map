94681df66cb1d924f955a59c5ce903da530053d0
Michael Bauer <mihi@lo-res.org>
2012-09-04 15:16:36 +0200

    final version

--- map.js
+++ map.js
@@ -1,10 +1,21 @@
-
 function init() {
     var data={};
+    $.getJSON("http://netneutralitymap.org/json/countries.json",function
+    (results) { 
+        for (i in results) {
+                    var cc=results[i].cc
+                    // build the data object for vectorMap
+                    data[cc]=results[i].percent+1; 
+                }
+            console.log(data);    
     $("#worldmap").vectorMap({map:'world_mill_en',
-        series: {regions:[ {
-            values: data}]}
-            });
+           series: {regions:[ {
+               values: data,
+               scale: ["#EEEEEE","#000000"]
+               }]}
+               });
+                
+                });
     }
 
 $(document).ready(init);    
